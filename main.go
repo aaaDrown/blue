259a1e9b929d86db34ca14530bbe551d6bbfb870
aaaDrown <1662326564@qq.com>
2023-11-15 11:35:01 +0800

    fix

--- main.go
+++ main.go
@@ -1,32 +1,41 @@
 package main
 
 import (
-	"blue/entity"
 	"blue/initialize"
 	"github.com/gin-gonic/gin"
-	"net/http"
 	"os"
 )
 
-// 跨域访问：cross  origin resource share
+//// 跨域访问：cross  origin resource share
+//func CrosHandler() gin.HandlerFunc {
+//	return func(context *gin.Context) {
+//		method := context.Request.Method
+//		context.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+//		context.Header("Access-Control-Allow-Origin", "*") // 设置允许访问所有域
+//		context.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE,UPDATE")
+//		context.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Token,session,X_Requested_With,Accept, Origin, Host, Connection, Accept-Encoding, Accept-Language,DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type, Pragma,token,openid,opentoken")
+//		context.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers,Cache-Control,Content-Language,Content-Type,Expires,Last-Modified,Pragma,FooBar")
+//		context.Header("Access-Control-Max-Age", "172800")
+//		context.Header("Access-Control-Allow-Credentials", "false")
+//		context.Set("content-type", "application/json")
+//
+//		if method == "OPTIONS" {
+//			context.JSON(http.StatusOK, entity.Response{StatusCode: 0, StatusMsg: "Options Request!"})
+//		}
+//
+//		//处理请求
+//		context.Next()
+//	}
+//}
+
 func CrosHandler() gin.HandlerFunc {
-	return func(context *gin.Context) {
-		method := context.Request.Method
-		context.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		context.Header("Access-Control-Allow-Origin", "*") // 设置允许访问所有域
-		context.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE,UPDATE")
-		context.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Token,session,X_Requested_With,Accept, Origin, Host, Connection, Accept-Encoding, Accept-Language,DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type, Pragma,token,openid,opentoken")
-		context.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers,Cache-Control,Content-Language,Content-Type,Expires,Last-Modified,Pragma,FooBar")
-		context.Header("Access-Control-Max-Age", "172800")
-		context.Header("Access-Control-Allow-Credentials", "false")
-		context.Set("content-type", "application/json")
-
-		if method == "OPTIONS" {
-			context.JSON(http.StatusOK, entity.Response{StatusCode: 0, StatusMsg: "Options Request!"})
-		}
-
-		//处理请求
-		context.Next()
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*") // 可将将 * 替换为指定的域名
+		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Next()
 	}
 }
 
